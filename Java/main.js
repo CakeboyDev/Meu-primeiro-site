1ab68f20bd23279254113b3f9de0d005d6f8e8db
CakeboyDev <emervegam@gmail.com>
2021-04-06 20:19:14 -0300

    Versão 0.3

--- Java/main.js
+++ Java/main.js
@@ -1,1 +1,18 @@
+//var idade = prompt("Quantos anos menó?");
 
+//if(idade<18)
+//{
+  //alert("Di menó");
+//}
+//else
+//{
+  //alert("Dentro da lei");
+//}
+
+function caqui(){
+document.getElementById("jamanta").innerHTML="selocooooooo";
+}
+function ciqua()
+{
+document.getElementById("jamanta").innerHTML="A Matilha";
+}
